8973a3beba342f1b0a233945e97ee226ffe267f0
Md.Samsul Arefin <samsularefin532@gmail.com>
2024-08-22 21:48:56 +0600

    Handle Student Schedule- M34V9

--- src/pages/students/MySchedule.tsx
+++ src/pages/students/MySchedule.tsx
@@ -1,5 +1,30 @@
+import { useGetAllOfferedCoursesQuery } from "../../redux/features/student/studentCourseManagement";
+
 const MySchedule = () => {
-  return <div></div>;
+  const { data } = useGetAllOfferedCoursesQuery(undefined);
+  return (
+    <div>
+      {data?.data?.map((item) => {
+        return (
+          <div>
+            {data?.data?.map((item) => {
+              return (
+                <div>
+                  <div>{item.course.title}</div>
+                  <div>{item.offeredCourse.section}</div>
+                  <div>
+                    {item.offeredCourse.days.map((item) => (
+                      <span> {item}</span>
+                    ))}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default MySchedule;
